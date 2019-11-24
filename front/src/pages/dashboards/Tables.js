406df1656306bfe64a094357456f22bd23d09099
hongnhung <hongnhunglyzin97@gmail.com>
2019-11-25 00:48:26 +0700

    updated

--- front/src/pages/dashboards/Tables.js
+++ front/src/pages/dashboards/Tables.js
@@ -260,8 +260,8 @@ class Tables extends React.Component {
                   condensed // responsive for table
                   striped
                   pagination={paginationFactory({
-                    sizePerPage: 5,
-                    sizePerPageList: [5, 10, 15, 20]
+                    sizePerPage: 3,
+                    sizePerPageList: [3, 6, 9]
                   })}
                   noDataIndication="Table is Empty"
                   hover
