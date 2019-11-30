39daada2a9a46c7e086d7348d8faa2d72f3a0260
hongnhung <hongnhunglyzin97@gmail.com>
2019-11-30 21:23:51 +0700

    format tieng viet trang admin

--- front/src/pages/admin/Admin.js
+++ front/src/pages/admin/Admin.js
@@ -475,10 +475,10 @@ class Admin extends React.Component {
 	render() {
 		return (
 			<Tabs defaultActiveKey="setting" >
-				<Tab eventKey="setting" title="Cài đặt">
+				<Tab eventKey="setting" title="Setting">
 					<General />
 				</Tab>
-				<Tab eventKey="members" title="Thành viên">
+				<Tab eventKey="members" title="Members">
 					<Members />
 				</Tab>
 			</Tabs>
